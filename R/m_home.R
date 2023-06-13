00e73918e8db77c47fa0e813bb25042b7a7ab332
Eshta Bhardwaj <eshta.bhardwaj@gmail.com>
2023-06-13 13:32:00 -0400

    text and logo changes for curbcut toronto

--- R/m_home.R
+++ R/m_home.R
@@ -7,34 +7,11 @@ home_UI <- function(id) {
     susPageSectionFeature(
       susCarousel(
         susCarouselSlide(
-          title = tags$em(nowrap("Centraide x"), 
-                          nowrap("Curbcut")),
-          preview = curbcut::cc_t("Centraide"),
-          tags$p(curbcut::cc_t("In a novel collaboration, Centraide of Greater ",
-                      "Montreal is partnering with Curbcut on a series of ",
-                      "housing maps. Centraide is using its social expertise ",
-                      "and data to help target and interpret housing issues, ",
-                      "a decisive factor in poverty and social exclusion.")),
-          tags$div(class = "sus-button-group",
-                   tags$a(class = "sus-button sus-icon-button sus-button-secondary", 
-                          href = "#", onclick = "openTab('afford')",
-                          curbcut::cc_t("Housing affordability"), 
-                          curbcut::icon_material("location_city")),
-                   tags$a(class = "sus-button sus-icon-button sus-button-primary", 
-                          href = "https://www.centraide-mtl.org/", 
-                          curbcut::cc_t("Centraide"), target = "_blank",
-                          img(src = "centraide_logo/centraide_sm.png",
-                              style = paste0("display:inline; height:20px; ",
-                                             "float:right; margin-top:auto; ",
-                                             "margin-bottom:auto; margin-left:10px")))
-          )
-        ),
-        susCarouselSlide(
           title = tags$em(nowrap(curbcut::cc_t("Towards a")), 
                           nowrap(curbcut::cc_t("sustainable city"))),
           preview = curbcut::cc_t("Welcome"),
           tags$p(curbcut::cc_t("Curbcut is a platform for exploring urban ",
-                              "sustainability in the Montreal region across ",
+                              "sustainability across ",
                               "multiple spatial and temporal scales. Curbcut ",
                               "offers a justice- and inclusivity-focused ",
                               "approach to sustainability which integrates ",
@@ -49,23 +26,6 @@ home_UI <- function(id) {
                          href = "#start-exploring", 
                          curbcut::cc_t("Start Exploring Maps"), 
                          curbcut::icon_material("travel_explore"))
-          )
-        ), susCarouselSlide(
-          title = tags$em(tags$em(nowrap(curbcut::cc_t("Get notified about")), 
-                                  nowrap(curbcut::cc_t("the 2021 Census")))),
-          preview = curbcut::cc_t("2021 Census data"),
-          tags$p(curbcut::cc_t("Many of the topics that can be explored on Curbcut draw on data from ",
-                      "the Canadian Census. The Census of Population is conducted ",
-                      "every five years and provides statistical information ",
-                      "about demographic, social and economic characteristics.")),
-          tags$p(curbcut::cc_t("Sign up to our newsletter to get notified when ",
-                      "the 2021 Census data is available on Curbcut!")),
-          tags$div(class = "sus-button-group",
-                   tags$a(class = "sus-button sus-icon-button sus-button-primary",
-                          style = "cursor: pointer;",
-                          id = "sign_up_from_carousel", 
-                          curbcut::cc_t("Sign up!"),
-                          curbcut::icon_material("email"))
           )
         )
       )
@@ -91,8 +51,7 @@ home_UI <- function(id) {
         "What is the relationship between heat risk and ",
         "housing tenure? Does my neighbourhood have better ",
         "or worse active transport options than the rest ",
-        "of the city? What is the history of environmental ",
-        "gentrification in Little Burgundy? The majority ",
+        "of the city? The majority ",
         "of the data is publicly available, and over time ",
         "we will be adding more tools for users to export ",
         "the data and use it themselves.")),
@@ -107,9 +66,15 @@ home_UI <- function(id) {
         curbcut::cc_t(" page to learn more about our team.")))),
       tags$p(class = "text-center", tags$em(
         curbcut::cc_t("An initiative of the "),
+        HTML(paste0("<a href = 'https://dci.ischool.utoronto.ca/'>",
+                    curbcut::cc_t("Digital Curation Institute,"), 
+                    "</a>")),
+      )),
+      tags$p(class = "text-center", tags$em(
+        curbcut::cc_t("In collaboration with the"),
         HTML(paste0("<a href = 'https://www.mcgill.ca/mssi/'>",
                     curbcut::cc_t("McGill Sustainability Systems Initiative"), 
-                    "</a>."))
+                    "</a>.")),
       ))
     ),
     susPageSection(
@@ -121,7 +86,7 @@ home_UI <- function(id) {
           ready_modules_home(mods_rdy), 
           list(linkListGroup(
             name = curbcut::cc_t("More"), 
-            list(name = curbcut::cc_t("Montréal stories"), 
+            list(name = curbcut::cc_t("Toronto stories"), 
                  onclick = "openTab('stories')"),
             list(name = curbcut::cc_t("Place explorer"), 
                  onclick = "openTab('place_explorer')"))))
